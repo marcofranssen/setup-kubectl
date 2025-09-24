4d79e82f3d3455c2e9e5d067f039acef8f6e2a0e
Marco Franssen <marco.franssen@gmail.com>
2025-09-25 00:19:23 +0200

    Update dist

--- dist/281.index.js
+++ dist/281.index.js
@@ -1,16 +1,15 @@
-export const id = 37;
-export const ids = [37];
+export const id = 281;
+export const ids = [281];
 export const modules = {
 
-/***/ 4037:
+/***/ 3281:
 /***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
 
-__webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "toFormData": () => (/* binding */ toFormData)
+/* harmony export */   toFormData: () => (/* binding */ toFormData)
 /* harmony export */ });
-/* harmony import */ var fetch_blob_from_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2777);
-/* harmony import */ var formdata_polyfill_esm_min_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(8010);
+/* harmony import */ var fetch_blob_from_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(6287);
+/* harmony import */ var formdata_polyfill_esm_min_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(8390);
 
 
 
@@ -366,7 +365,7 @@ async function toFormData(Body, ct) {
 	let contentType;
 	let filename;
 	const entryChunks = [];
-	const formData = new formdata_polyfill_esm_min_js__WEBPACK_IMPORTED_MODULE_1__/* .FormData */ .Ct();
+	const formData = new formdata_polyfill_esm_min_js__WEBPACK_IMPORTED_MODULE_1__/* .FormData */ .fS();
 
 	const onPartData = ui8a => {
 		entryValue += decoder.decode(ui8a, {stream: true});
@@ -377,7 +376,7 @@ async function toFormData(Body, ct) {
 	};
 
 	const appendFileToFormData = () => {
-		const file = new fetch_blob_from_js__WEBPACK_IMPORTED_MODULE_0__/* .File */ .$B(entryChunks, filename, {type: contentType});
+		const file = new fetch_blob_from_js__WEBPACK_IMPORTED_MODULE_0__/* .File */ .ZH(entryChunks, filename, {type: contentType});
 		formData.append(entryName, file);
 	};
 
@@ -449,4 +448,4 @@ async function toFormData(Body, ct) {
 
 };
 
-//# sourceMappingURL=37.index.js.map
+//# sourceMappingURL=281.index.js.map
